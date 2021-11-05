cfff2dd36a50311a07cd3ff6390493e36f6fe06f
Bruna Ishida <bruishida2107@gmail.com>
2021-11-04 23:54:00 -0300

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val titular = "Alex"
+    val conta = 1000
+    val saldo = 302.0
+
+    println("Bem Vindo ao ByteBank")
+    println("titular $titular")
+    println("número da conta $conta")
+    println("saldo da conta $saldo")
+}
