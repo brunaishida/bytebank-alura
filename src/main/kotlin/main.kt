bb4565521d53ce3bce2bae3aa23c28dd82070f48
Bruna Ishida <bruishida2107@gmail.com>
2021-11-05 00:09:49 -0300

    add: method validation saldo

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,21 @@
 fun main() {
     val titular = "Alex"
     val conta = 1000
-    val saldo = 302.0
+    var saldo = 0.0
+    saldo += 300 + 2.0
 
     println("Bem Vindo ao ByteBank")
     println("titular $titular")
     println("número da conta $conta")
     println("saldo da conta $saldo")
+
+    testaCondicao(saldo)
+}
+
+fun testaCondicao(saldo: Double) {
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> println("conta é negativa")
+    }
 }
